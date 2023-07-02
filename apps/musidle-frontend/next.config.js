41bdffb691f3bc3f9fddbfc0939acb2a39fff434
Highlighted-dev <bartosz.korz@gmail.com>
2023-07-02 18:30:05 +0200

    Edited nextjs config

--- apps/musidle-frontend/next.config.js
+++ apps/musidle-frontend/next.config.js
@@ -1,3 +1,24 @@
+//@ts-check
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { composePlugins, withNx } = require('@nx/next');
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+};
+
+const plugins = [
+  // Add more Next.js plugins to this list if needed.
+  withNx({ nextConfig }),
+];
+
 module.exports = {
   async rewrites() {
     return [
@@ -7,4 +28,5 @@ module.exports = {
       },
     ];
   },
+  plugins,
 };

7a61e54414df608113871948b31d0358efa79b64
Highlighted-dev <bartosz.korz@gmail.com>
2023-11-25 20:17:28 +0100

    Updated the main page

--- apps/musidle-frontend/app/page.tsx
+++ apps/musidle-frontend/app/page.tsx
@@ -1,10 +1,42 @@
-'use client';
-
 import GameSingleplayerLayout from '@/components/singleplayer/GameSingleplayerLayout';
-export default function Index() {
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import Link from 'next/link';
+export default async function Page() {
   return (
-    <div className="rounded-md overflow-hidden w-4/6 h-4/6 min-h-[600px]">
-      <GameSingleplayerLayout />
+    <div className="h-4/5 w-[90%] flex xl:flex-row xl:relative flex-col justify-center align-center relative min-h-[450px]">
+      <Card className="float-left xl:w-4/6 flex flex-col justify-center align-center h-full">
+        <div className="h-full">
+          <CardHeader className="text-center h-1/6">
+            <CardTitle>Welcome to Musidle</CardTitle>
+            <CardDescription>
+              Guess today&apos;s music and challenge your knowledge!
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid xl:grid-cols-2 grid-cols-1 gap-2 h-5/6">
+            <Card className="flex flex-col justify-center items-center p-8 h-full">
+              <CardTitle className="text-sm text-center">
+                Challange your music knowledge with new songs every day
+              </CardTitle>
+              <CardContent className="flex flex-col justify-center items-center">
+                <Button className="w-[250px] m-4" variant={'outline'}>
+                  <Link href={'/singleplayer'}>Singleplayer</Link>
+                </Button>
+              </CardContent>
+            </Card>
+            <Card className="flex flex-col justify-center items-center p-8 h-full">
+              <CardTitle className="text-sm text-center">
+                Play with your friends to see who is the real music guru
+              </CardTitle>
+              <CardContent className="flex flex-col justify-center items-center">
+                <Button className="w-[250px] m-4" variant={'outline'}>
+                  <Link href={'/multiplayer'}>Multiplayer</Link>
+                </Button>
+              </CardContent>
+            </Card>
+          </CardContent>
+        </div>
+      </Card>
     </div>
   );
 }

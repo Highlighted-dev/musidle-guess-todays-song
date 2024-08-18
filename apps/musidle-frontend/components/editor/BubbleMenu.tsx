3ab295359d948d5b2cfb993fe4c37d217d7c1fb5
Highlighted-dev <bartosz.korz@gmail.com>
2024-08-18 19:28:39 +0200

    Updated BubbleMenu styles

--- apps/musidle-frontend/components/editor/BubbleMenu.tsx
+++ apps/musidle-frontend/components/editor/BubbleMenu.tsx
@@ -1,34 +1,51 @@
-import { Editor, BubbleMenu } from '@tiptap/react';
+import { type Editor, BubbleMenu } from '@tiptap/react';
 import { Button } from '../ui/button';
 import { LuBold, LuItalic, LuStrikethrough } from 'react-icons/lu';
-export function EditorBubbleMenu({ editor }: { editor: Editor }) {
+
+export function EditorBubbleMenu({ editor }: { editor: Editor | null }) {
+  if (!editor) {
+    return null;
+  }
+
   return (
     <BubbleMenu
       editor={editor}
       tippyOptions={{ duration: 100 }}
-      className="flex justify-center items-center bg-popover border h-8 p-1 rounded-md rounded-br-none rounded-bl-none border-input flex-row gap-1"
+      className="inline-flex h-10 flex-row items-center justify-center gap-0.5 rounded-lg border border-neutral-200 bg-white p-1 leading-none shadow-sm dark:border-neutral-800 dark:bg-black"
     >
       <Button
         onClick={() => editor.chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active h-6 p-2 ' : 'h-6 p-2 '}
-        variant={'secondary'}
+        className={`h-8 p-2 ${
+          editor.isActive('bold')
+            ? 'bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-800 dark:hover:bg-neutral-700'
+            : ''
+        }`}
         type={'button'}
+        variant={'ghost'}
       >
         <LuBold size={20} />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active h-6 p-2 ' : 'h-6 p-2 '}
-        variant={'secondary'}
+        className={`h-8 p-2 ${
+          editor.isActive('italic')
+            ? 'bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-800 dark:hover:bg-neutral-700'
+            : ''
+        }`}
         type={'button'}
+        variant={'ghost'}
       >
         <LuItalic size={20} />
       </Button>
       <Button
         onClick={() => editor.chain().focus().toggleStrike().run()}
-        className={editor.isActive('strike') ? 'is-active h-6 p-2' : 'h-6 p-2'}
-        variant={'secondary'}
+        className={`h-8 p-2 ${
+          editor.isActive('strike')
+            ? 'bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-800 dark:hover:bg-neutral-700'
+            : ''
+        }`}
         type={'button'}
+        variant={'ghost'}
       >
         <LuStrikethrough size={20} />
       </Button>
